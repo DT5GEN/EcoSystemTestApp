0f4fb05f50012ff50b123e437e42fcfef15e97ea
Alx_Shilov <deeppowercrew@gmail.com>
2025-01-11 20:43:35 +0300

    working history v3.2 and 2 card

--- app/src/main/java/com/dt5gen/ecosystem/ui/MainScreen.kt
+++ app/src/main/java/com/dt5gen/ecosystem/ui/MainScreen.kt
@@ -1,19 +1,27 @@
 package com.dt5gen.ecosystem.ui
 
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import com.dt5gen.ecosystem.domain.models.BinInfoResponse
 import com.dt5gen.ecosystem.presentation.bin.BinViewModel
 
 @Composable
@@ -22,6 +30,7 @@ fun MainScreen(
     onNavigateToHistory: () -> Unit
 ) {
     val binInfoState by viewModel.binInfo.collectAsState()
+    val errorMessage by viewModel.errorMessage.collectAsState()
     // val binInput = remember { mutableStateOf("") }
 
     Column(
@@ -76,13 +85,94 @@ fun MainScreen(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(vertical = 8.dp),
-                elevation = CardDefaults.cardElevation(4.dp)
+                elevation = CardDefaults.cardElevation(8.dp)
             ) {
                 Column(modifier = Modifier.padding(16.dp)) {
-                    Text("Phone: ${binInfo.bank?.phone ?: "N/A"}")
+                    Text("Тип карты: ${binInfo.type ?: "N/A"}")
+                    Text("Платёжная система: ${binInfo.brand ?: "N/A"}")
                     Text("Страна: ${binInfo.country?.name ?: "N/A"}")
-                    Text("Банк: ${binInfo.bank?.name ?: "N/A"}")
+                    Text("Банк: ${binInfo.bank?.name?: "N/A"}")
+                    Text("Город: ${binInfo.bank?.city ?: "N/A"}")
+                    Text("Телефон: ${binInfo.bank?.phone ?: "N/A"}")
+                    Text("сайт: ${binInfo.bank?.phone ?: "N/A"}")
                 }
+            }
+        }
+
+        // Карточка с результатами
+        ResultCard(info = binInfoState, errorMessage = errorMessage)
+    }
+}
+
+@Composable
+fun ResultCard(
+    info: BinInfoResponse?,
+    errorMessage: String?
+) {
+    val context = LocalContext.current // Получаем контекст внутри @Composable функции
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 36.dp),
+        shape = RoundedCornerShape(8.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            if (!errorMessage.isNullOrEmpty()) {
+                Text(
+                    text = errorMessage,
+                    color = Color.Red,
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(bottom = 8.dp)
+                )
+            } else if (info != null) {
+                Text("Страна: ${info.country?.name ?: "N/A"}")
+                info.country?.latitude?.let { lat ->
+                    info.country.longitude?.let { lng ->
+                        Text(
+                            text = "Координаты: $lat, $lng",
+                            color = Color.Blue,
+                            modifier = Modifier.clickable {
+                                val uri = Uri.parse("geo:$lat,$lng")
+                                val intent = Intent(Intent.ACTION_VIEW, uri).apply {
+                                    setPackage("com.google.android.apps.maps")
+                                }
+                                context.startActivity(intent)
+                            }
+                        )
+                    }
+                }
+                Text("Банк: ${info.bank?.name ?: "N/A"}")
+                info.bank?.url?.let { url ->
+                    Text(
+                        text = "URL: $url",
+                        color = Color.Blue,
+                        modifier = Modifier.clickable {
+                            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                            context.startActivity(intent)
+                        }
+                    )
+                }
+                info.bank?.phone?.let { phone ->
+                    Text(
+                        text = "Телефон: $phone",
+                        color = Color.Blue,
+                        modifier = Modifier.clickable {
+                            val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phone"))
+                            context.startActivity(intent)
+                        }
+                    )
+                }
+            } else {
+                Text(
+                    text = "Введите данные и нажмите Поиск",
+                    color = Color.Gray
+                )
             }
         }
     }
